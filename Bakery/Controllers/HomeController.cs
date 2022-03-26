fefb9a10856ea7c44402062dac0b17e698811f22
plymatea <andy.plymate@gmail.com>
2022-03-25 18:16:27 -0700

    WIP passing dictionary to home/index view

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,14 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Bakery.Models;
 
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/")]
-    public ActionResult Index()
+    private readonly BakeryContext _db;
+
+    public HomeController( BakeryContext db)
     {
-      ViewBag.PageTitle = "Welcome to Pierre's Bakery!!";
-      return View();
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index(string searchString)
+    {
+    var flavors = from m in _db.Flavors
+                select m;
+    var treats = from m in _db.Treats
+                select m;
+      if (!String.IsNullOrEmpty(searchString))
+      {
+        flavors = flavors.Where(s => s.Name!.Contains(searchString));
+        treats = treats.Where(s => s.Name!.Contains(searchString));
+        flavors.ToList();
+        treats.ToList();
+      }
+    Dictionary<string, List<Flavor>> flavorsTreats = new Dictionary<string, List<Flavor>>();
+    flavorsTreats.Add("flavors", flavors);
+    return View(flavorsTreats);
     }
   }
 }
---
+++ Bakery/ViewModels/DictionaryViewModel.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+public class DictionaryView
+{
+    public Dictionary<string, string> dictionary { get; set; }
+}
