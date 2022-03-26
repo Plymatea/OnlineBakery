f6bf806ae155bb6398113922531c5835fba39e36
plymatea <andy.plymate@gmail.com>
2022-03-25 19:04:39 -0700

    Still WIP on passing flavors and treat into home page.

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -17,23 +17,53 @@ namespace Bakery.Controllers
       _db = db;
     }
 
+    // [HttpGet("/")]
+    // public async Task<IActionResult> Index(string searchString)
+    // {
+    // var flavorsTreats = from m in _db.FlavorTreat
+    //   .Include(m => m.Treat)
+    //   .Include(m => m.Flavor)
+    //   select m;
+    //   {
+    //   if (!String.IsNullOrEmpty(searchString))  
+    //     flavorsTreats = flavorsTreats.Where(s => s.Flavor.Name!.Contains(searchString));
+    //     flavorsTreats = flavorsTreats.Where(s => s.Treat.Name!.Contains(searchString));
+    //   }
+
+    // return View(await flavorsTreats.ToListAsync());
+    // }
+
+    // [HttpGet("/")]
+    // public async Task<IActionResult> Index(string searchString)
+    // {
+    // var flavors = from m in _db.Flavors
+    //   select m;
+    //   {
+    //   if (!String.IsNullOrEmpty(searchString))  
+    //     flavors = flavors.Where(s => s.Name!.Contains(searchString));
+    //   }
+
+    // return View(await flavors.ToListAsync());
+    // }
+
     [HttpGet("/")]
-    public ActionResult Index(string searchString)
+    public async Task<IActionResult> Index(string searchString)
     {
     var flavors = from m in _db.Flavors
-                select m;
+      select m;
     var treats = from m in _db.Treats
-                select m;
-      if (!String.IsNullOrEmpty(searchString))
+      select m; 
       {
+      if (!String.IsNullOrEmpty(searchString))  
         flavors = flavors.Where(s => s.Name!.Contains(searchString));
         treats = treats.Where(s => s.Name!.Contains(searchString));
-        flavors.ToList();
-        treats.ToList();
+        await flavors.ToListAsync();
+        await treats.ToListAsync();
       }
-    Dictionary<string, List<Flavor>> flavorsTreats = new Dictionary<string, List<Flavor>>();
-    flavorsTreats.Add("flavors", flavors);
-    return View(flavorsTreats);
+    Dictionary<string, object> flavorstreats = new Dictionary<string, object>();
+    flavorstreats.Add("flavors", flavors);
+    flavorstreats.Add("treats", treats);
+    return View(flavorstreats);
     }
   }
 }
--- Bakery/ViewModels/FlavorTreatViewModel.cs
+++ Bakery/ViewModels/FlavorTreatViewModel.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-public class DictionaryView
+public class FlavorTreatView
 {
     public Dictionary<string, string> dictionary { get; set; }
 }
